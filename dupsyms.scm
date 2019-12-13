1bfac85a9c53b83cd6d8688d6456baf966ee0c4b
Hamayama <hamay1010@gmail.com>
2019-12-13 21:56:01 +0900

    first commit

---
+++ dupsyms.scm
@@ -0,0 +1,47 @@
+;; -*- coding: utf-8 -*-
+;;
+;; dupsyms.scm
+;; 2019-12-13 v1.00
+;;
+;; ＜内容＞
+;;   Gauche で、import されたシンボルの重複チェックを行うための
+;;   モジュールです。
+;;
+;;   詳細については、以下のページを参照ください。
+;;   https://github.com/Hamayama/dupsyms
+;;
+(define-module dupsyms
+  (use gauche.interactive)
+  (export
+    dupsyms))
+(select-module dupsyms)
+
+;; check if the imported duplicate symbols exist
+(define-syntax dupsyms
+  (syntax-rules ()
+    [(_) (%dupsyms (current-module))]))
+(define (%dupsyms module)
+  (define imported-list '())
+  (with-input-from-string
+      (with-output-to-string
+        (^[] (with-module gauche.interactive (%apropos #/.*/ module #f))))
+    (^[]
+      (let loop ([sym (read)])
+        (unless (eof-object? sym)
+          (let ([mod (read)])
+            (push! imported-list (cons sym mod))
+            (loop (read)))))))
+  (get-duplicates (reverse imported-list) equal? car))
+
+;; get the list consisting only of duplicate elements
+;;  e.g. '(1 2 2 3 4 4 4 5 6) ==> '(2 2 4 4 4)
+(define (get-duplicates list :optional (cmpfn equal?) (keyfn identity))
+  (let loop ([list list] [ret '()] [hit-prev #f])
+    (if (null? list)
+      (reverse ret)
+      (let ([hit (and (not (null? (cdr list)))
+                      (cmpfn (keyfn (car list)) (keyfn (cadr list))))])
+        (loop (cdr list)
+              (if (or hit hit-prev) (cons (car list) ret) ret)
+              hit)))))
+
---
+++ test.scm
@@ -0,0 +1,41 @@
+;;
+;; Test dupsyms
+;;
+
+(add-load-path "." :relative)
+(use gauche.test)
+
+(test-start "dupsyms")
+(use dupsyms)
+(test-module 'dupsyms)
+
+(define (filter-by-module imported-list mod-list)
+  (filter
+   (^[item] (member (cadr item) mod-list))
+   imported-list))
+
+(define-module mod-A
+  (export aaa bbb f1)
+  (define aaa 1000)
+  (define bbb 2000)
+  (define (f1 x) (+ x 10)))
+
+(define-module mod-B
+  (export aaa ccc f1)
+  (define aaa 3000)
+  (define ccc 4000)
+  (define (f1 x y) (+ x y)))
+
+(import mod-A)
+(import mod-B)
+
+(test-section "dupsyms")
+(test* "dupsyms-1" 
+       '((aaa mod-A) (aaa mod-B) (f1 mod-A) (f1 mod-B))
+       (filter-by-module (dupsyms) '(mod-A mod-B)))
+
+;; summary
+(format (current-error-port) "~%~a" ((with-module gauche.test format-summary)))
+
+(test-end)
+
